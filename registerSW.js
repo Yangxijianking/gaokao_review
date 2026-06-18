4edfc6fd56758ee91b0482f5f766bc42a65ded87
GaoKao Review <gaokao@example.com>
2026-06-18 17:50:36 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/gaokao_review/sw.js', { scope: '/gaokao_review/' })})}
